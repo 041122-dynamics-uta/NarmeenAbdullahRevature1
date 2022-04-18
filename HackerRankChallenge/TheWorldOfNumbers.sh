9f4278e5dbbd80229b5d751b26c2fdd341952589
Narmeen <narmeen428@revature.net>
2022-04-18 09:12:03 -0400

    Looping With  Numbers

--- HackerRankChallenge/TheWorldOfNumbers.sh
+++ HackerRankChallenge/TheWorldOfNumbers.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+#Given two integers,  and , find their sum, difference, product, and quotient.
+
+#Input Format
+
+#Two lines containing one integer each ( and , respectively).
+
+#Constraints
+
+
+
+#Output Format
+
+#Four lines containing the sum (), difference (), product (), and quotient (), respectively.
+#(While computing the quotient, print only the integer part.)
 echo Enter Number1
 read x
 echo Enter Number2
