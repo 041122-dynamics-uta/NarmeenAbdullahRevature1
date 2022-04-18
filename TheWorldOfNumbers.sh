74afb8a267c693fdbb7bfe15e37bcc8b539139e9
Narmeen <narmeen428@revature.net>
2022-04-18 07:11:32 -0400

    The World Of Numbers

--- TheWorldOfNumbers.sh
+++ TheWorldOfNumbers.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+echo Enter Number1
+read x
+echo Enter Number2
+read y
+echo Sum is:
+expr $x + $y
+echo Difference is: $(( x - y ))
+expr $x - $y
+echo Product is: $(( x * y ))
+expr $x \* $y
+echo Quotiont is: $(( x / y ))
+expr $x / $y
+
+
+
+
