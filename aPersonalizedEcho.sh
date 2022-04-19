735494b3b7c9f2025e19a6f9613e1228b97bf6ea
Narmeen <narmeen428@revature.net>
2022-04-19 02:10:08 -0400

    HackerRankChallenge2

---
+++ aPersonalizedEcho.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Write a Bash script which accepts  as input and displays the greeting "Welcome (name)"
+#Input Format
+#There is one line of text,name
+
+#Solution:
+#let the computer read input and then print Welcome input
+
+read input
+echo Welcome ${input}
+
