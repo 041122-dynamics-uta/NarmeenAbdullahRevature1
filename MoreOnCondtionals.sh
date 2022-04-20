f4115f8c9918b898fb8aa85bb8459976225c4c3a
Narmeen <narmeen428@revature.net>
2022-04-20 02:32:38 -0400

    updateMoreOnConditionals

--- MoreOnCondtionals.sh
+++ MoreOnCondtionals.sh
@@ -15,9 +15,22 @@
 #One word: either "SCALENE" or "EQUILATERAL" or "ISOSCELES" (quotation marks excluded).
 
 
+
 read X
 read Y
 read Z
+
+if [ $X -eq $Y ] || [ $X -eq $Z ] || [ $Z -eq $Y ];
+then
+    if [ $((X+Y)) -eq $((Z*2)) ];
+    then
+        echo "EQUILATERAL"
+    else
+        echo "ISOSCELES"
+        fi
+else
+    echo "SCALENE"
+fi 
 
 
     
