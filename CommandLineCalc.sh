528d6b81c36bd42f23594af72fe7c945686d1958
Narmeen <narmeen428@revature.net>
2022-04-25 03:58:14 -0400

    Add functions

--- CommandLineCalc.sh
+++ CommandLineCalc.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 #Now Pseudocode
 #This program allow User to choose one of four different calculations
-#Then it asks user to enter two number to execute the chosen calculation 
+#Then it asks user to enter two number to execute the chosen calculation
 #if choice=1 then print $(( num + num2 ))
 #if choice=2 print $(( num - num2 ))
 #if choice=3 print $(( num * num2 ))
 #if choice=4 print $(( num / num2 ))
 #Ask user if want to continue, if yes do again .. if no exit and done
-#use a loop 
+#use functions for calculation types Add(), Subtract (), Multiply(), Divide()
 
 
 echo "                                                     "
 echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo "                                                     "
-echo "Welcome to Narmeens Calculator"
+echo "HEY! WHATS YOUR NAME PLEASE?"
 echo "                                                     "
-echo "I Have 4 Calculation Options for you:"
+read name
 echo "                                                     "
-
+echo "HELLO $name, WELCOME TO COMMAND LINE CALCULATOR"
+echo "                                                     "
+echo "CHOOSE ONE OF THE FOLLOWING FOUR CALCULATIONS"
+echo "                                                     "
+#Line-25
 Menu () {
 
 echo "Enter 1 to Add"
@@ -27,15 +31,31 @@ echo "Enter 3 to Multiply"
 echo "Enter 4 to Divide"
 echo "                                                              "
 
+Addition () {
+        echo "The Result of Add Operation is:"
+        expr $n1 + $n2
+}
+
+Subtraction () {
+        echo "The Result of Subtract Operation is:"
+        expr $n1 - $n2
+}
+
+Multiplication () {
+        echo "The Result of Multiply Operation is:"
+        expr $n1 \* $n2
+}
+
+
+Division () {
+        echo "The Result of Divide Operation is:"
+        expr $n1 / $n2
+}
+
+
+
 read choice
 
-#echo "Enter The First Number"
-#read n1
-
-#echo "Enter The Second Number"
-#read n2
-
-#__________________________________________________________________________________
 
 if [ $choice == 1 ]
 then
@@ -45,13 +65,11 @@ read n1
 echo "Enter The Second Number"
 read n2
 
-echo "The Result of Add Operation is:" 
-expr $n1 + $n2
-fi
+Addition
+echo The Result of Add Operation is: $expr
 
-#__________________________________________________________________________________
 
-if [ $choice == 2 ]
+elif [ $choice == 2 ]
 then
 echo "Enter The First Number"
 read n1
@@ -59,14 +77,27 @@ read n1
 echo "Enter The Second Number"
 read n2
 
-echo "The Result of Subtract Operation is:"
-expr $n1 - $n2
+Subtraction
 
-fi
 
-#__________________________________________________________________________________
 
-if [ $choice == 3 ]
+
+
+elif [ $choice == 3 ]
+	then
+echo "Enter The First Number"
+read n1
+
+echo "Enter The Second Number"
+read n2
+
+Multiplication
+
+
+
+
+
+elif [ $choice == 4 ]
 then
 echo "Enter The First Number"
 read n1
@@ -74,67 +105,120 @@ read n1
 echo "Enter The Second Number"
 read n2
 
-	echo "The Result of Multiply Operation is:"
-expr $n1 \* $n2
+Division
+
+
+
+
+
+else
+
+        echo "                                                             "
+        echo "THIS IS NOT A VALID OPTION .. PLEASE INPUT OPTION FROM 1 TO 4"
+        Menu
 fi
+
+
+
+
+#echo "Enter The Second Number"
+#read n2
+
+#echo "The Result of Add Operation is:"
+#expr $n1 + $n2
+
 
 #__________________________________________________________________________________
 
-if [ $choice == 4 ]
-then
-echo "Enter The First Number"
-read n1
-
-echo "Enter The Second Number"
-read n2
-
-	echo "The Result of Divide Operation is:"
-expr $n1 / $n2
-fi
-
-#__________________________________________________________________________________
-
-
-
-#	echo "Please Input A Choice From 1 to 4"
-#	Menu
-
+#elif [ $choice == 2 ]
+#then
 #echo "Enter The First Number"
 #read n1
 
 #echo "Enter The Second Number"
 #read n2
 
+#echo "The Result of Subtract Operation is:"
+#expr $n1 - $n2
 
+
+
+#__________________________________________________________________________________
+
+#elif [ $choice == 3 ]
+#then
+#echo "Enter The First Number"
+#read n1
+#read n2
+
+#       echo "The Result of Multiply Operation is:"
+#expr $n1 \* $n2
+
+
+#__________________________________________________________________________________
+
+#elif [ $choice == 4 ]
+#then
+#echo "Enter The First Number"
+#read n1
+
+#echo "Enter The Second Number"
+#read n2
+#Line-90
+#       echo "The Result of Divide Operation is:"
+#expr $n1 / $n2
+
+#__________________________________________________________________________________
+
+#else
+
+#       echo "                                                             "
+#       echo "THIS IS NOT A VALID OPTION .. PLEASE INPUT OPTION FROM 1 TO 4"
+#       Menu
+#fi
+
+
+
+Show () {
+
+if [ $decesion == "YES" ] || [ $decesion == "yes" ] || [ $decesion == "Yes" ]
+then
+        echo "                                                   "
+        echo "Continuing Now"
+        echo "                                                   "
+        echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+   echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+        echo "                                                      "
+
+        Menu
+
+
+elif [ $decesion == "no" ] || [ $decesion == "NO" ] || [ $decesion == "No" ]
+then
+        echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+        echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+        echo "                                                      "
+        echo "Sorry to see you leave, Bye!!!"
+
+exit
+
+
+else
+        echo "Please choose either to continue or to stop by typing Yes or No "
+        read decesion
+
+        Show
+fi
+}
 
 echo "Do you Want to Make Another Calculation? Yes or No"
 read decesion
-
-if [ $decesion == "YES" ] || [ $decesion == "yes" ]
-then 
-	echo "                                                   "
-	echo "Continuing Now"
-	echo "                                                   "
-	Menu
-fi 
+Show
 
 
-if [ $decesion == "no" ] || [ $decesion == "NO" ]
-echo " I'm Sorry to see you leave Bye!"
-then
-exit	
-fi
+
 
 }
-
 Menu
-
-echo "                           "
-echo "This Is Not A Valid Choice"     #if user did not enter choice [1..4]
-echo "                           "
-echo "Please Input A Choice From 1 to 4"
-       Menu
-
-echo " Thank you, Wish you All The Best"
 
 
