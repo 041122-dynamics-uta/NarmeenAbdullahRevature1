9be799c3c894831156d815497af433e21684dd33
Narmeen <narmeen428@revature.net>
2022-05-13 20:45:11 -0400

    adding client layer

---
+++ P1/CosmoStarClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CosmoStarClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
